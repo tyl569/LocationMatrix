e0a5973fc0ba569e483788819fc3faa9b72d260a
tyl569 <tengyunlong@feilong.tech>
2020-08-06 17:58:20 +0800

    refactor: 修改写法

--- Location.php
+++ Location.php
@@ -27,7 +27,7 @@ class Location
         $sinU1 = $tanU1 * $cosU1;
         $sigma1 = atan2($tanU1, $cosAlpha1);
         $sinAlpha = $cosU1 * $sinAlpha1;
-        $cosSqAlpha = 1 - $sinAlpha * $sinAlpha;
+        $cosSqAlpha = 1 - pow($sinAlpha, 2);
         $uSq = $cosSqAlpha * (self::A * self::A - self::B * self::B) / (self::B * self::B);
         $A = 1 + $uSq / 16384 * (4096 + $uSq * (-768 + $uSq * (320 - 175 * $uSq)));
         $B = $uSq / 1024 * (256 + $uSq * (-128 + $uSq * (74 - 47 * $uSq)));
@@ -38,19 +38,19 @@ class Location
             $cos2SigmaM = cos(2 * $sigma1 + $sigma);
             $sinSigma = sin($sigma);
             $cosSigma = cos($sigma);
-            $deltaSigma = $B * $sinSigma * ($cos2SigmaM + $B / 4 * ($cosSigma * (-1 + 2 * $cos2SigmaM * $cos2SigmaM) -
-                        $B / 6 * $cos2SigmaM * (-3 + 4 * $sinSigma * $sinSigma) * (-3 + 4 * $cos2SigmaM * $cos2SigmaM)));
+            $deltaSigma = $B * $sinSigma * ($cos2SigmaM + $B / 4 * ($cosSigma * (-1 + 2 * pow($cos2SigmaM, 2)) -
+                        $B / 6 * $cos2SigmaM * (-3 + 4 * pow($sinSigma, 2)) * (-3 + 4 * pow($cos2SigmaM, 2))));
             $sigmaP = $sigma;
             $sigma = $distance / (self::B * $A) + $deltaSigma;
         }
 
         $tmp = $sinU1 * $sinSigma - $cosU1 * $cosSigma * $cosAlpha1;
         $lat2 = atan2($sinU1 * $cosSigma + $cosU1 * $sinSigma * $cosAlpha1,
-            (1 - self::F) * sqrt($sinAlpha * $sinAlpha + $tmp * $tmp));
+            (1 - self::F) * sqrt(pow($sinAlpha, 2) + pow($tmp, 2)));
         $lambda = atan2($sinSigma * $sinAlpha1, $cosU1 * $cosSigma - $sinU1 * $sinSigma * $cosAlpha1);
         $C = self::F / 16 * $cosSqAlpha * (4 + self::F * (4 - 3 * $cosSqAlpha));
         $L = $lambda - (1 - $C) * self::F * $sinAlpha *
-            ($sigma + $C * $sinSigma * ($cos2SigmaM + $C * $cosSigma * (-1 + 2 * $cos2SigmaM * $cos2SigmaM)));
+            ($sigma + $C * $sinSigma * ($cos2SigmaM + $C * $cosSigma * (-1 + 2 * pow($cos2SigmaM, 2))));
 
         return [
             'lng' => $lng + rad2deg($L),
